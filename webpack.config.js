d0d149187dee1bd31d2571a89102896144367c5a
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-16 22:10:26 +0100

    add minification to UMD min build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV === "production";
+
+const plugins = isProduction
+  ? [
+      new UglifyJsPlugin(),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("production")
+      })
+    ]
+  : [];
 
 module.exports = {
   entry: "./lib/commonjs/index.js",
@@ -8,6 +19,7 @@ module.exports = {
     library: "CTFLWizard",
     libraryTarget: "umd",
     path: path.resolve(__dirname, "dist"),
-    filename: `cfl-webpack-wizard.${isProduction ? "min." : ""}js`
-  }
+    filename: `contentful-wizard.${isProduction ? "min." : ""}js`
+  },
+  plugins
 };
